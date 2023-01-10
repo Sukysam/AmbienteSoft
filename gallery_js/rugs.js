01b18e2e6ff1f72b18e72c14432214adb5e42a3a
Samuel Bolarinwa <sukysamcy@gmail.com>
2023-01-10 14:14:45 +0100

    final

---
+++ gallery_js/rugs.js
@@ -0,0 +1,103 @@
+let galList = document.querySelectorAll(".gall_list");
+let getLatesOpededImg;
+let windowWidth = window.innerWidth;
+
+if (galList) {
+    galList.forEach(function(image, index) {
+        image.onclick = function() {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgPos = getFullImgUrl.split("/img/product/thumbs/");
+            let setNewImgUrl = getImgPos[1].replace('")', "");
+
+            getLatesOpededImg = index + 1;
+
+            let gallContainer = document.body;
+            let newImgWindow = document.createElement("div");
+            gallContainer.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+
+            let newImg = document.createElement("img");
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute("src", "../../img/product/rugs/" + setNewImgUrl);
+            newImg.setAttribute("id", "current-img");
+
+            newImg.onload = function() {
+                let imgWidth = this.width;
+                let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
+
+                let newNextBtn = document.createElement("a");
+                let btnNextText = document.createTextNode("Next");
+                newNextBtn.appendChild(btnNextText);
+                gallContainer.appendChild(newNextBtn);
+                newNextBtn.setAttribute("class", "img-btn-next");
+                newNextBtn.setAttribute("onclick", "changeImg(1)");
+                newNextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
+
+                let newPrevBtn = document.createElement("a");
+                let btnPrevText = document.createTextNode("Prev");
+                newPrevBtn.appendChild(btnPrevText);
+                gallContainer.appendChild(newPrevBtn);
+                newPrevBtn.setAttribute("class", "img-btn-prev");
+                newPrevBtn.setAttribute("onclick", "changeImg(0)");
+                newPrevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
+            }
+
+        }
+    });
+}
+
+function closeImg() {
+    document.querySelector(".img-window").remove();
+    document.querySelector(".img-btn-next").remove();
+    document.querySelector(".img-btn-prev").remove();
+}
+
+function changeImg(changeDir) {
+    document.querySelector("#current-img").remove();
+
+    let getImgWindow = document.querySelector(".img-window");
+    let newImg = document.createElement("img");
+    getImgWindow.appendChild(newImg);
+
+    let calcNewImg;
+    if (changeDir === 1) {
+        calcNewImg = getLatesOpededImg + 1;
+        if (calcNewImg > galList.length) {
+            calcNewImg = 1;
+        }
+
+    } else if (changeDir === 0) {
+        calcNewImg = getLatesOpededImg - 1;
+        if (calcNewImg < 1) {
+            calcNewImg = galList.length;
+        }
+    }
+
+    newImg.setAttribute("src", "../../img/product/beds/bed" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/pillows/t-pill" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/dinings/din" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/contracts/cont" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/curtains/cur" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/lamps/lamp" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/sofas/sofa" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/rugs/rug" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/fabrics/fab" + calcNewImg + ".png");
+    //newImg.setAttribute("src", "../../img/product/mirrors/mir" + calcNewImg + ".png");
+    newImg.setAttribute("id", "current-img");
+
+    getLatesOpededImg = calcNewImg;
+
+    newImg.onload = function() {
+        let imgWidth = this.width;
+        let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
+
+        let nextBtn = document.querySelector(".img-btn-next");
+        nextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
+
+        let prevBtn = document.querySelector(".img-btn-prev");
+        prevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
+
+    }
+}
